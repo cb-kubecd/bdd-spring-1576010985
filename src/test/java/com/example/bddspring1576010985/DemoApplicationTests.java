a1ab6cfb009c8331afb2bc12ee61b97f8ca07c29
CJXDBot <jenkins-x@googlegroups.com>
2019-12-10 20:49:49 +0000

    Initial import

---
+++ src/test/java/com/example/bddspring1576010985/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bddspring1576010985;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
